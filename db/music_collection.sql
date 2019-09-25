bbb62a60ea89dfc815b588f6ba638450f85911d7
Alex Forrester <alexforrester96@gmail.com>
2019-09-25 14:21:19 +0100

    created sql file and sql runner

---
+++ db/music_collection.sql
@@ -0,0 +1,14 @@
+DROP TABLE IF EXISTS artists;
+DROP TABLE IF EXISTS albums;
+
+CREATE TABLE artists (
+  name VARCHAR(255),
+  id SERIAL8 PRIMARY KEY
+)
+
+CREATE TABLE ALBUMN (
+  title VARCHAR(255),
+  genre VARCHAR(255),
+  id SERIAL8 PRIMARY KEY,
+  artist_id INT8 REFERENCES artists(id)
+)
